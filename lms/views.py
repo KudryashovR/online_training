abde39f696600bef8553e72b02c5829989e53e77
KudryashovR <romkud55@gmail.com>
2024-08-05 22:03:12 +0300

    ADD: Creating Generic classes for editing a user profile

--- lms/views.py
+++ lms/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from lms.models import Course, Lesson
-from lms.serializers import CourseSerializer, LessonSerializer
+from lms.serializers import CourseSerializer, LessonSerializer, UserProfileSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -17,3 +19,12 @@ class LessonListCreateAPIView(generics.ListCreateAPIView):
 class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+
+class UserProfileRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
