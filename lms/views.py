4a589e469f730ee80d0275352740e3ae51baef7a
KudryashovR <romkud55@gmail.com>
2024-08-05 21:34:41 +0300

    ADD: Creating Generic classes for lessons

--- lms/views.py
+++ lms/views.py
@@ -1,9 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
-from lms.models import Course
-from lms.serializers import CourseSerializer
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class LessonListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
