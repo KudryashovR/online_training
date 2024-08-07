cb634ceac57a173d00a82556c6853d733f4b5d24
KudryashovR <romkud55@gmail.com>
2024-08-07 22:59:27 +0300

    FIX: search filter for Payment

--- config/settings.py
+++ config/settings.py
@@ -139,10 +139,3 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.CustomUser'
-
-REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-        'django_filters.rest_framework.OrderingFilter',
-    ]
-}
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
@@ -24,5 +25,6 @@ class PaymentViewSet(viewsets.ModelViewSet):
 
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ['paid_course', 'paid_lesson', 'payment_method', 'payment_date']
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['paid_course', 'paid_lesson', 'payment_method']
+    ordering_fields = ['payment_date']
