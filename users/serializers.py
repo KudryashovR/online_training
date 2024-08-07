002780f13c924afd4af3c705f297383440b1ed92
KudryashovR <romkud55@gmail.com>
2024-08-07 23:09:17 +0300

    ADD: Extending the user profile serializer to include payment history

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from users.models import Payment
+from users.models import Payment, CustomUser
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -18,4 +18,32 @@ class PaymentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Payment
+        fields = '__all__'
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор профиля пользователя. Этот класс предназначен для преобразования данных профиля пользователя в формат
+    JSON и обратно.
+
+    Атрибуты
+    --------
+    payment_history : PaymentSerializer
+        Сериализатор для вывода истории платежей пользователя. Использует сериализатор PaymentSerializer
+        для отображения связанных платежей. Поле many=True указывает, что может быть несколько платежей, read_only=True
+        делает это поле только для чтения, а source='payments' указывает на имя обратного отношения в модели платежей.
+
+    Метакласс
+    ---------
+    Meta
+        model : CustomUser
+            Указывает модель, с которой работает данный сериализатор.
+        fields : str
+            Поле 'all' указывает, что необходимо сериализовать все поля модели CustomUser.
+    """
+
+    payment_history = PaymentSerializer(many=True, read_only=True, source='payments')
+
+    class Meta:
+        model = CustomUser
         fields = '__all__'
