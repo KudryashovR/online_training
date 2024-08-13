42cc0c925187246ea70a85ec05ff580744284086
KudryashovR <romkud55@gmail.com>
2024-08-13 21:21:34 +0300

    ADD: Updating controllers

--- lms/views.py
+++ lms/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from lms.models import Course, Lesson
 from lms.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModerator
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     """
+    UPDATE
     Предоставляет стандартные действия `CRUD` для модели Course.
 
     Атрибуты:
@@ -18,9 +21,17 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_permissions(self):
+        if self.action in ['destroy', 'create']:
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update', 'retrieve', 'list']:
+            self.permission_classes = [IsAuthenticated, IsModerator]
+        return [permission() for permission in self.permission_classes]
+
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):
     """
+    UPDATE
     Предоставляет действия `список` и `создание` для модели Lesson.
 
     Атрибуты:
@@ -33,9 +44,17 @@ class LessonListCreateAPIView(generics.ListCreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [IsAuthenticated]
+        elif self.action == 'list':
+            self.permission_classes = [IsAuthenticated, IsModerator]
+        return [permission() for permission in self.permission_classes]
+
 
 class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     """
+    UPDATE
     Предоставляет действия `получение`, `обновление` и `удаление` для модели Lesson.
 
     Атрибуты:
@@ -47,3 +66,10 @@ class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+    def get_permissions(self):
+        if self.action == 'destroy':
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update', 'retrieve']:
+            self.permission_classes = [IsAuthenticated, IsModerator]
+        return [permission() for permission in self.permission_classes]
