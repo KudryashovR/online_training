9e60bf52fdeda2e96b60ccfd6be7a3089bffe6d6
KudryashovR <romkud55@gmail.com>
2024-08-22 20:41:36 +0300

    ADD: validation to serializer

--- lms/validators.py
+++ lms/validators.py
@@ -8,8 +8,13 @@ class LinkValidator:
     Валидатор, который разрешает только ссылки на youtube.com.
     """
 
+    message = 'Можно добавлять только ссылки на youtube.com.'
+
+    def __init__(self, field):
+        self.field = field
+
     def __call__(self, value):
         parsed_url = urlparse(value)
 
         if parsed_url.netloc != 'www.youtube.com' and parsed_url.netloc != 'youtube.com':
-            raise ValidationError('Можно добавлять только ссылки на youtube.com.')
+            raise ValidationError(self.message)
