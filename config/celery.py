4d69ef37639a2d828b9480af102b3832d454fe8a
KudryashovR <romkud55@gmail.com>
2024-08-26 21:35:51 +0300

    FIX: Settings for Celery and Celery beat

--- config/celery.py
+++ config/celery.py
@@ -2,9 +2,9 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_training.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('online_training')
+app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
