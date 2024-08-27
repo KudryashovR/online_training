4667521f00929536d614d5e29cd8043c014a2adf
KudryashovR <romkud55@gmail.com>
2024-08-27 20:25:35 +0300

    ADD: Setting up Celery

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -9,3 +10,10 @@ app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks(['lms', 'users'])
+
+app.conf.beat_schedule = {
+    'deactivate-inactive-users': {
+        'task': 'lms.tasks.deactivate_inactive_users',
+        'schedule': crontab(hour=0, minute=0),
+    },
+}
