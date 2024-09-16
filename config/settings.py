9fcdde14f5c17d413652d5cd9f5b9b1eb97e4b87
KudryashovR <159808397+KudryashovR@users.noreply.github.com>
2024-09-17 02:31:19 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('POSTGRES_DB'),
-        'USER': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASSWORD'),
         'HOST': env('DATABASES_HOST'),
     }
